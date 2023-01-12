516446d2b5841fc9b36837d9877834509ad7352d
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2023-01-12 12:57:08 +0530

    update

--- social-react/src/components/leftbar/LeftBar.jsx
+++ social-react/src/components/leftbar/LeftBar.jsx
@@ -1,9 +1,111 @@
-import './leftBar.scss'
+import "./leftBar.scss";
 
 const LeftBar = () => {
   return (
-    <div className='leftBar'>LeftBar</div>
-  )
-}
+    <div className="leftBar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+              alt=""
+            />
+            <span>Somnath</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1533/1533763.png"
+              alt=""
+            />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7051/7051303.png"
+              alt=""
+            />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7857/7857762.png"
+              alt=""
+            />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7245/7245112.png"
+              alt=""
+            />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3066/3066491.png"
+              alt=""
+            />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your Shortcuts</span>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/8927/8927109.png"
+              alt=""
+            />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3097/3097980.png"
+              alt=""
+            />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2659/2659360.png"
+              alt=""
+            />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2839/2839026.png"
+              alt=""
+            />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/9187/9187469.png"
+              alt=""
+            />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src="https://cdn-icons-png.flaticon.com/512/2037/2037747.png" alt="" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src="https://cdn-icons-png.flaticon.com/512/748/748602.png" alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src="https://cdn-icons-png.flaticon.com/512/4412/4412834.png" alt="" />
+            <span>Courses</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LeftBar
+export default LeftBar;
--- social-react/src/components/rightbar/RightBar.jsx
+++ social-react/src/components/rightbar/RightBar.jsx
@@ -2,7 +2,33 @@ import './rightBar.scss'
 
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPZRlDLLRvB5Qnb1gjwLZmzdopBRgIZ2yPw&usqp=CAU" alt="" />
+              <span>Jiya Indore</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPZRlDLLRvB5Qnb1gjwLZmzdopBRgIZ2yPw&usqp=CAU" alt="" />
+              <span>Jiya Indore</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
