31ec44c9adc0838b1847c8d0477c234fd71340e0
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2023-01-16 15:43:40 +0530

    update

--- social-react/src/App.js
+++ social-react/src/App.js
@@ -5,7 +5,7 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import "./style.scss";
+import "./theme.scss";
 import {
   createBrowserRouter,
   Navigate,
